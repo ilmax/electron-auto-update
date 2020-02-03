d6c157b28dd75a455d02d6bc90079ccd2cf20cfa
Massimiliano Donini <massimiliano@ticketinggroup.com>
2020-02-03 22:49:24 +0100

    Move build configuration into json file

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,11 @@
+const config = require("./buildconfig.json");
+
 module.exports = {
   lintOnSave: false,
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
-        productName: "Electron-app-sample",
-        copyright: "Copyright © 2020 ${author}",
-        appId: "my-company.my-app",
-        artifactName: "${name}_${os}-setup.${ext}",
-        publish: [
-          {
-            provider: "generic",
-            url:
-              "https://tsdevstorage.blob.core.windows.net/temp-blob-container",
-            channel: "beta"
-          }
-        ],
-        win: {
-          compression: "store"
-        },
-        mac: {
-          compression: "store"
-        }
+        ...config
       }
     }
   }
