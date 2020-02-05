1bf3c8f01115901fad6ced09845d9b52f580bc48
Massimiliano Donini <massimiliano@ticketinggroup.com>
2020-02-05 21:06:06 +0100

    Change message to beta on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App latest" />
+    <HelloWorld msg="Welcome to Your Vue.js App beta" />
   </div>
 </template>
 
